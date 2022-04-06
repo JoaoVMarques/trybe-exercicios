3aa57352c12acfde733041456249bf696ab382ff
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-04-06 15:50:27 -0300

    exercicio 02.2

---
+++ 01-fundamentos/bloco-04/dia-01/exercicio-02-2.js
@@ -0,0 +1,12 @@
+// 1 - Crie uma costante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+const base = 5;
+let height = 8;
+
+// 2 - Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura.
+const area = (base * height)
+
+// 3 - Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+const perimeter = 5 + 5 + 8 + 8;
+
+console.log(area);
+console.log(perimeter);
