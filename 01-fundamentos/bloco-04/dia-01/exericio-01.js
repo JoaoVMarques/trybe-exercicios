e494fd2571f5ec72467ff6aa9b8908774d651936
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-04-06 15:29:55 -0300

    Exercicio 1

---
+++ 01-fundamentos/bloco-04/dia-01/exericio-01.js
@@ -0,0 +1,10 @@
+const myName = "João";
+const birthCity = "gravatai";
+let birthYear = 2003;
+
+birthYear = 2030;
+// birthCity = "São paulo" [Erro porque não é possivel mudar uma const]
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
