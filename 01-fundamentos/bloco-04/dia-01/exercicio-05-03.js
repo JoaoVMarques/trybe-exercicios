51e040d6b1d04f6a4341ea9a1755a0f11c638931
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-04-06 18:45:56 -0300

    programa 3

---
+++ 01-fundamentos/bloco-04/dia-01/exercicio-05-03.js
@@ -0,0 +1,13 @@
+const a = 2
+const b = 1
+const c = 3
+
+if (a > b && a > c) {
+  console.log("o 'a' é o maior número entre 'b' e 'c'.")
+}
+else if (b > a && b > c) {
+  console.log("o 'b' é o maior número entre 'a' e 'c'")
+}
+else if (c > a && c > b) {
+  console.log("o 'c' é o maior número entre 'a' e 'b'")
+}
