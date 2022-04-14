c0bc43c0e574ae93404d4be9c78713f5e2d90a4f
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-04-14 19:03:49 -0300

    Exercicio-02.1/02.2 bloco-05.2

---
+++ 01-fundamentos/bloco-02/dia-03/exercicio-02/script.js
@@ -0,0 +1,53 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const daysList = document.getElementById('days');
+
+function createList(days) {
+  const list = document.createElement("li");
+  const text = document.createTextNode(days)
+
+  if (days === 24 || days === 25 || days === 31) {
+    list.classList.add('holiday')
+  }
+
+  if (days === 4 || days === 11 || days === 18 || days === 25) {
+    list.classList.add('friday')
+  }
+  list.appendChild(text);
+
+  return list;
+}
+
+for(let days of dezDaysList) {
+  daysList.appendChild(createList(days));
+}
+
+// Criar dias ----------
+
+// Criar botoes ----------
+
+const buttonContainer = document.getElementsByClassName('buttons-container');
+
+function createHolidayButton (name) {
+  const btn = document.createElement('button');
+  btn.innerHTML = name;
+  btn.id = 'btn-holiday';
+  buttonContainer[0].appendChild(btn);
+}
+
+createHolidayButton('Feriados');
