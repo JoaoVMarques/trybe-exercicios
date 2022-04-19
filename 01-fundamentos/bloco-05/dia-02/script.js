b1dfca6522f2e342240e70e29c621e1b54fd28b2
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-04-19 15:46:37 -0300

    Resolução exercicio/02-02

--- 01-fundamentos/bloco-05/dia-02/script.js
+++ 01-fundamentos/bloco-05/dia-02/script.js
@@ -1,4 +1,8 @@
 body = document.getElementsByTagName('body')[0];
-let title = document.createElement('h1')
+let title = document.createElement('h1');
 title.innerText = 'Exercício 5.2 - JavaScript DOM';
 body.appendChild(title);
+
+let main = document.createElement('main');
+main.classList.add('main-content');
+body.appendChild(main);
