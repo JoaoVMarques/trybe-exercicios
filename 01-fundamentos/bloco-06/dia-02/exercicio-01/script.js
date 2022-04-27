3f109feda70e59d1a9587ca8009ae41e752d1d4a
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-04-27 19:16:14 -0300

    melhoria no formulario e calendario

--- 01-fundamentos/bloco-06/dia-02/exercicio-01/script.js
+++ 01-fundamentos/bloco-06/dia-02/exercicio-01/script.js
@@ -1,10 +1,12 @@
-document.getElementById('send-button').addEventListener('click', (event) => {
-  event.preventDefault()
+document.getElementById('submit-btn').addEventListener('click', (event) => {
+  console.log('enviado');
 });
+// const validate = new window.JustValidate('#form');
 
 const myDate = new Date();
 const picker = new Pikaday({
    field: document.getElementById('datepicker'),
+   trigger: document.getElementById('selectData'),
    minDate: new Date(myDate.getFullYear(), myDate.getMonth() + 1, 1),
+   defaultDate: new Date(myDate.getFullYear(), myDate.getMonth() + 1, 1),
    maxDate: new Date(2022, 11, 31) });
-console.log(myDate.getDay());
