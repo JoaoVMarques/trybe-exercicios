18f13d98f21af87efa69fcb1055ed5c3bd957f15
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-04-27 18:43:29 -0300

    Utilizando Pikaday para arrumar o calendario

--- 01-fundamentos/bloco-06/dia-02/exercicio-01/script.js
+++ 01-fundamentos/bloco-06/dia-02/exercicio-01/script.js
@@ -1,3 +1,10 @@
 document.getElementById('send-button').addEventListener('click', (event) => {
   event.preventDefault()
 });
+
+const myDate = new Date();
+const picker = new Pikaday({
+   field: document.getElementById('datepicker'),
+   minDate: new Date(myDate.getFullYear(), myDate.getMonth() + 1, 1),
+   maxDate: new Date(2022, 11, 31) });
+console.log(myDate.getDay());
