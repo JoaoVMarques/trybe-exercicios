6e279dc9c5b483c72104461fa13e3a6e8b751667
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-05-02 15:41:32 -0300

    Exercicio 1 bloco-06.2

--- 01-fundamentos/bloco-06/dia-02/exercicio-01/script.js
+++ 01-fundamentos/bloco-06/dia-02/exercicio-01/script.js
@@ -1,12 +1,53 @@
-document.getElementById('submit-btn').addEventListener('click', (event) => {
-  console.log('enviado');
+document.getElementById('datepicker').addEventListener('click', (event) => {
+  event.preventDefault();
 });
+
 // const validate = new window.JustValidate('#form');
 
 const myDate = new Date();
 const picker = new Pikaday({
-   field: document.getElementById('datepicker'),
-   trigger: document.getElementById('selectData'),
-   minDate: new Date(myDate.getFullYear(), myDate.getMonth() + 1, 1),
-   defaultDate: new Date(myDate.getFullYear(), myDate.getMonth() + 1, 1),
-   maxDate: new Date(2022, 11, 31) });
+  field: document.getElementById('datepicker'),
+  trigger: document.getElementById('selectData'),
+  minDate: new Date(myDate.getFullYear(), myDate.getMonth() + 1, 1),
+  defaultDate: new Date(myDate.getFullYear(), myDate.getMonth() + 1, 1),
+  maxDate: new Date(2022, 11, 31)
+});
+
+const validation = new JustValidate('#form');
+validation
+  .addField('#name', [
+    {
+      rule: 'required',
+      errorMessage: 'Name is required',
+    },
+    {
+      rule: 'minLength',
+      value: 10,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ])
+  .addField('#datepicker', [
+    {
+      rule: 'required',
+      errorMessage: 'Data is required',
+    },
+  ])
+  .addField('#use-pictures', [
+    {
+      rule: 'required',
+      errorMessage: 'Term is required',
+    },
+]);
