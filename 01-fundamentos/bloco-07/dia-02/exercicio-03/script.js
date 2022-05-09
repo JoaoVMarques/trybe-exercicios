bf7918b24e3495b892db8694c3141fb24f009c2f
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-05-09 20:17:51 -0300

    Exercicio 3 Completo Bloco-07.2

--- 01-fundamentos/bloco-07/dia-02/exercicio-03/script.js
+++ 01-fundamentos/bloco-07/dia-02/exercicio-03/script.js
@@ -37,6 +37,38 @@ function listValueObject(object) {
   console.log(Object.values(object));
 }
 
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+// console.log(allLessons);
+
+function numStudents() {
+  let students = 0;
+  const obj = Object.keys(allLessons);
+  for (let i = 0; i < Object.keys(allLessons).length; i += 1) {
+    students += (allLessons[obj[i]].numeroEstudantes);
+  }
+  console.log(students);
+}
+
+function getValueByNumber(object, key) {
+  console.log(Object.values(object)[key]);
+}
+
+function verifyPair(object, key, value) {
+  const arr = Object.entries(object);
+
+  let pair = false;
+  for (let i in arr) {
+    if (arr[i][0] === key && arr[i][1] === value ) {
+      pair = true;
+    }
+  }
+  console.log(pair);
+}
+
 listKeyObject(lesson2);
 listValueObject(lesson2);
 ObjectLength(lesson2);
+numStudents();
+getValueByNumber(lesson2, 0);
+verifyPair(lesson3, 'turno', 'noite');
+verifyPair(lesson3, 'materia', 'Maria Clara');
