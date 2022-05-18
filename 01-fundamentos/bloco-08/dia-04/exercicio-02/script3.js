978110ef3b78f5c47fb89c3485d8a3c54bbdf65e
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-05-18 19:41:37 -0300

    Refatorando exercicio 5 bloco-08.4

--- 01-fundamentos/bloco-08/dia-04/exercicio-02/script3.js
+++ 01-fundamentos/bloco-08/dia-04/exercicio-02/script3.js
@@ -4,14 +4,12 @@ const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 const avarageGradeMath = (index) => grades[index]
   .reduce((acc, curr) => acc + curr, 0) / grades[index].length;
 
-function studentAverage() {
-  return studentGrades = students.map((person, index) => {
+const studentAverage = () => students.map((person, index) => {
     const avarageGrade = avarageGradeMath(index);
     return {
       name: person,
       average: avarageGrade
     }
   })
-}
 
 console.log(studentAverage());
