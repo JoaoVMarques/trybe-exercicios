befa388ff3331a6da4ad2da6697a96a1085ca788
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-05-18 19:18:08 -0300

    Exercicio-05 Completo Bloco-08.4

---
+++ 01-fundamentos/bloco-08/dia-04/exercicio-02/script3.js
@@ -0,0 +1,17 @@
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const avarageGradeMath = (index) => grades[index]
+  .reduce((acc, curr) => acc + curr, 0) / grades[index].length;
+
+function studentAverage() {
+  return studentGrades = students.map((person, index) => {
+    const avarageGrade = avarageGradeMath(index);
+    return {
+      name: person,
+      average: avarageGrade
+    }
+  })
+}
+
+console.log(studentAverage());
