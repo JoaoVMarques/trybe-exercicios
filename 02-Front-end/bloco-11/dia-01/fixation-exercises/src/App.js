24b2d63e0d6d60b51478de8a020bec35c8413da1
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-06-22 16:31:13 -0300

    Exercicio-Fixação-3 Bloco-11-01

--- 02-Front-end/bloco-11/dia-01/fixation-exercises/src/App.js
+++ 02-Front-end/bloco-11/dia-01/fixation-exercises/src/App.js
@@ -6,16 +6,39 @@ class App extends Component {
     super()
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+    this.state = {
+      clicksButton1: 0,
+      clicksButton2: 0,
+      clicksButton3: 0,
+    }
   }
 
   handleClick() {
-    console.log(this);
+    this.setState((oldState, _props) => ({
+      clicksButton1: oldState.clicksButton1 +1
+    }))
+  }
+
+  handleClick2() {
+    this.setState((oldState, _props) => ({
+      clicksButton2: oldState.clicksButton2 +1
+    }))
+  }
+
+  handleClick3() {
+    this.setState((oldState, _props) => ({
+      clicksButton3: oldState.clicksButton3 +1
+    }))
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>Meu botão</button>
+        <button onClick={this.handleClick}>{this.state.clicksButton1}</button>
+        <button onClick={this.handleClick2}>{this.state.clicksButton2}</button>
+        <button onClick={this.handleClick3}>{this.state.clicksButton3}</button>
       </div>
     )
   }
