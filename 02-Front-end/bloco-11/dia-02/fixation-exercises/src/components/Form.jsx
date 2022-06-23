0ebf7e3f39556850c0811bb7ecebdf4621540559
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-06-23 16:23:12 -0300

    Exercicio de fixação 2 Bloco-11.02

--- 02-Front-end/bloco-11/dia-02/fixation-exercises/src/components/Form.jsx
+++ 02-Front-end/bloco-11/dia-02/fixation-exercises/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import '../styles/form.css'
 
 class Form extends Component {
   constructor() {
@@ -7,13 +8,20 @@ class Form extends Component {
     this.handleChange = this.handleChange.bind(this);
 
     this.state = {
-      comidaFavorita: '',
-    }
+      comidaFavorita: "",
+      idade: 0,
+      personagemFavorito: "mario",
+      texto: "",
+      gostaDeGolfinhos: false,
+    };
   }
 
-  handleChange(event) {
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+
     this.setState({
-      comidaFavorita: event.target.value,
+      [name]: value,
     });
   }
 
@@ -22,25 +30,65 @@ class Form extends Component {
       <div>
         <h1>Formulario super bacana B)</h1>
         <form>
-          <label>
-            Comida favorita:
-            <input value={this.state.comidaFavorita} onChange={this.handleChange} type="text" placeholder="nham nham" />
-          </label>
-          <label>
-            Idade:
-            <input type="number" placeholder="0" />
-          </label>
-          <label>
-            Personagem favorito:
-            <select>
-              <option value="mario">Mario</option>
-              <option value="luigi">Luigi</option>
-              <option value="yoshi">Yoshi</option>
-            </select>
-          </label>
+          <fieldset>
+            <label>
+              Comida favorita:
+              <input
+                name="comidaFavorita"
+                value={this.state.comidaFavorita}
+                onChange={this.handleChange}
+                type="text"
+                placeholder="nham nham"
+              />
+            </label>
+            <label>
+              Idade:
+              <input
+                name="idade"
+                value={this.state.idade}
+                onChange={this.handleChange}
+                type="number"
+                placeholder="0"
+              />
+            </label>
+            <label>
+              Personagem favorito:
+              <select
+                name="personagemFavorito"
+                value={this.state.personagemFavorito}
+                onChange={this.handleChange}
+              >
+                <option value="mario">Mario</option>
+                <option value="luigi">Luigi</option>
+                <option value="yoshi">Yoshi</option>
+              </select>
+            </label>
+          </fieldset>
+          <fieldset>
+            <label>
+              Gosta de golfinhos?
+              <input
+                name="gostaDeGolfinhos"
+                value={this.state.gostaDeGolfinhos}
+                onChange={this.handleChange}
+                type="checkbox"
+              />
+            </label>
+            <label>
+              Qual pudim mais bonito voce ja viu?
+              <input type="file" />
+            </label>
+          </fieldset>
           <label>
             Escreva algo legal:
-            <textarea cols="30" rows="4" placeholder="Use suas melhores palavras :)"></textarea>
+            <textarea
+              name="texto"
+              value={this.state.texto}
+              onChange={this.handleChange}
+              cols="30"
+              rows="4"
+              placeholder="Use suas melhores palavras :)"
+            ></textarea>
           </label>
         </form>
       </div>
