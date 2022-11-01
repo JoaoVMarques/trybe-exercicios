186e76fef2349b71a7115f47ee169e0c995b3a84
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-11-01 15:45:34 -0300

    Exercicio de fixação 1 Bloco-26.2

---
+++ 03-Back-end/bloco-26/dia-02/fixation-01/types.ts
@@ -0,0 +1,23 @@
+type bird = {
+  wings: 2;
+  beak: 1;
+}
+
+type sum = (x: number, y: number) => number;
+
+type adress = {
+  state: string;
+  city: string;
+  number: number;
+  publicPlace: boolean;
+}
+
+// --- parte 2 --- //
+
+type state = "liquid" | "solid" | "gaseous";
+
+type document = number | string;
+
+type system = "linux" | "mac os" | "windows";
+
+type vogals = "a" | "e" | "i" | "o" | "u";
