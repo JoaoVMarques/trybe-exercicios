1d0b63d2547697d5b89f9f58292924ce82312324
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2023-03-01 19:59:59 -0300

    Exercicio 02 Concluido

---
+++ 04-Ciencia-da-computacao/secao-02/dia-02/exercicio-02.py
@@ -0,0 +1,35 @@
+class Carta:
+    def __init__(self, valor, naipe):
+        self.valor = valor
+        self.naipe = naipe
+
+    def __repr__(self):
+        return '<%s de %s>' % (self.valor, self.naipe)
+    
+class Iterador:
+    def __init__(self, cartas):
+        self.cartas = cartas
+        self._pos = 0
+
+    def __next__(self):
+        try:
+            carta = self.cartas[self._pos]
+        except IndexError:
+            raise StopIteration()
+        else:
+            self._pos += 1
+            return carta
+
+class Baralho:
+    naipes = 'copas ouros espadas paus'.split()
+    valores = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split()
+
+    def __init__(self):
+        self._cartas = [
+            Carta(valor, naipe)
+            for naipe in self.naipes
+            for valor in self.valores
+        ]
+
+    def __len__(self):
+        return len(self._cartas)
